02bf9946f1bd83710ae7a3c65a6e895ccccd9f06
Eric Brian Njuguna <brianerick.njuguna@gmail.com>
2025-03-04 09:54:49 +0300

    Added functions that mutate and unmutate the employee object

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Brian",
+    streetAddress: "NBI CBD",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const copyOfEmployeeObj = { ...employee };
+    copyOfEmployeeObj[key] = value;
+    return copyOfEmployeeObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const copyOfEmployeeObj = { ...employee };
+    delete copyOfEmployeeObj[key];
+    return copyOfEmployeeObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+console.log(destructivelyDeleteFromEmployeeByKey(employee, 'name'));
+console.log(employee)
